69a2612af09d5172a816f5a44dc3262a17219bd9
bruceaudo <audo401@gmail.com>
2023-03-16 15:04:10 +0300

    Setting up authorization code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config'
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [AuthModule, PostsModule, PrismaModule, ConfigModule.forRoot({
     isGlobal: true
-  })
+  }), UsersModule
 ],
 })
 export class AppModule {}
